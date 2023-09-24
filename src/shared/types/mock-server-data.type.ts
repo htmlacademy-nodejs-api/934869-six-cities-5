2da50fcd77a9ae6a53492da5a7effbc71245f034
TarasovAndrej <tarasov.a@rusrobots.ru>
2023-09-24 13:22:30 +0300

    create mock server data

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,13 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  citys: string[];
+  previewImages: string[];
+  images: string[][];
+  housingTypes: string[];
+  comforts: string[];
+  users: string[];
+  emails: string[];
+  avatars: string[];
+  coordinates: string[];
+};
